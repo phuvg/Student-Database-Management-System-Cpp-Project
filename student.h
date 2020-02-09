5d045a4f4caa2a4599fc57361aee5d4e2ca846e8
sondovan96 <60708028+sondovan96@users.noreply.github.com>
2020-02-10 01:10:12 +0700

    Update student.h

--- student.h
+++ student.h
@@ -1,5 +1,20 @@
 #pragma once
-class student
+#include "Ngay.h"
+#include<string>
+class Student
 {
+private:
+	string Name , Address;
+	int Code;
+	Birthday Birthday;
+public:
+	void Import();
+	void Export();
+	string getName();
+	string getAdd();
+	int getCode();
+	void setName(string nameStu);
+	void setAddr(string addrStu);
+	void setCode(int codeStu);
 };
 
