d9bde58db358e2e430445db437c59b1179061bfd
sondovan96 <60708028+sondovan96@users.noreply.github.com>
2020-02-10 01:10:14 +0700

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,1 +1,45 @@
 #include "student.h"
+
+void Student::Import()
+{
+	fflush(stdin);
+	cout << "\nStudent Name: ";
+	getline(cin, Name);
+	cout << "\nAddress: ";
+	getline(cin, Address);
+	cout << "\nBirthday: ";
+	Birthday.Import();
+	cout << "\nInput Student Code: ";
+}
+void Student::Export()
+{
+	cout << "\nStudent Code: " << Code;
+	cout << "\nStudent Name: " << Name;
+	cout << "\nBirthday: ";
+	Birthday.Export();
+	cout << "\nAddress: " << Address;
+}
+int Student::getCode()
+{
+	return Code;
+}
+string Student::getName()
+{
+	return Name;
+}
+string Student::getAdd()
+{
+	return Address;
+}
+void Student::setCode(int codeStu)
+{
+	Code = codeStu;
+}
+void Student::setName(string nameStu)
+{
+	Name = nameStu;
+}
+void Student::setAddr(string addrStu)
+{
+	Address = addrStu;
+}
